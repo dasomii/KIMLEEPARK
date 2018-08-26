fb799cba05168273219aead4858cacf39dbe3d5e
dasomii <39484100+dasomii@users.noreply.github.com>
2018-08-26 19:46:09 +0900

    Create rgbled_bluetooth_control.ino

---
+++ arduino/rgbled_bluetooth_control.ino
@@ -0,0 +1,149 @@
+#include <SoftwareSerial.h>
+
+SoftwareSerial BTSerial(3, 2); //arduino pin 2:rx 3:tx
+
+int led_H = 5;  //white
+int led_W = 6;  //warm white
+int led_R = 9;  //red
+int led_G = 10; //green
+int led_B = 11; //blue
+
+int H, W, R, G, B;
+
+int leds[] = {5,6,9,10,11};
+int brightness = 0;
+int i;
+char input = '7';
+
+void fadeIn(int pin, int brightness){
+  for(i=0; i<brightness; i++)
+  {
+    analogWrite(pin, i);
+    delay(10);
+  }
+}
+
+void fadeOut(int pin, int brightness){
+  for(i=brightness; i>=0; i--)
+  {
+    analogWrite(pin, i);
+    delay(10);
+  }
+}
+
+void setup() {
+  for(i=0; i<sizeof(leds); i++){
+    pinMode(leds[i], OUTPUT);
+  }
+  Serial.begin(9600);
+  BTSerial.begin(9600);
+}
+
+void loop() {
+  if(BTSerial.available()){
+    input = BTSerial.read();
+    Serial.write(input);
+    Serial.write('\n');
+    //delay(100);
+  }
+  if(Serial.available())
+    BTSerial.write(Serial.read());
+
+  if(input == '0'){
+    Serial.println("all turn off");
+    for(i=0; i<sizeof(leds); i++){
+      analogWrite(leds[i], 0);
+    }
+    H = 0;
+    W = 0;
+    R = 0;
+    G = 0;
+    B = 0;  
+  }
+
+  if(input == '1'){
+    Serial.println("white 100");
+    fadeIn(led_H, 100);
+    fadeOut(led_W, W);
+    fadeOut(led_R, R);  
+    fadeOut(led_G, G);  
+    fadeOut(led_B, B); 
+    H = 100;
+    W = 0;
+    R = 0;
+    G = 0;
+    B = 0;
+  }
+  
+  if(input == '2'){
+    Serial.println("white 255");
+    fadeIn(led_H, 255);
+    fadeOut(led_W, W);
+    fadeOut(led_R, R);  
+    fadeOut(led_G, G);  
+    fadeOut(led_B, B);
+    H = 255;
+    W = 0;
+    R = 0;
+    G = 0;
+    B = 0; 
+  }
+  
+  if(input == '3'){
+    Serial.println("warm white");
+    fadeOut(led_H, H);
+    fadeIn(led_W, 255);
+    fadeOut(led_R, R);  
+    fadeOut(led_G, G);  
+    fadeOut(led_B, B);
+    H = 0;
+    W = 255;
+    R = 0;
+    G = 0;
+    B = 0; 
+
+  }
+
+  if(input == '4'){
+    Serial.println("Red");
+    fadeOut(led_H, H);
+    fadeOut(led_W, W);
+    fadeIn(led_R, 255);  
+    fadeOut(led_G, G);  
+    fadeOut(led_B, B);
+    H = 0;
+    W = 0;
+    R = 255;
+    G = 0;
+    B = 0; 
+
+  }
+
+  if(input == '5'){
+    Serial.println("Green");
+    fadeOut(led_H, H);
+    fadeOut(led_W, W);
+    fadeOut(led_R, R);  
+    fadeIn(led_G, 255);  
+    fadeOut(led_B, B);
+    H = 0;
+    W = 0;
+    R = 0;
+    G = 255;
+    B = 0; 
+  }
+
+  if(input == '6'){
+    Serial.println("Blue");
+    fadeOut(led_H, H);
+    fadeOut(led_W, W);
+    fadeOut(led_R, R);  
+    fadeOut(led_G, G);  
+    fadeIn(led_B, 255);
+    H = 0;
+    W = 0;
+    R = 0;
+    G = 0;
+    B = 255;  
+  }  
+}
